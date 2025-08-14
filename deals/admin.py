aeb1c62ebec33e57b1c9433fafcaa14f1adef50f
SaleDrop-DEV <u4987625257@gmail.com>
2025-08-14 18:02:33 +0200

    Update admin.py

--- deals/admin.py
+++ deals/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import GmailMessage, GmailSaleAnalysis, Store, ScrapeData, SubscriptionData
+from .models import GmailMessage, GmailSaleAnalysis, Store, ScrapeData, SubscriptionData, Url, GmailToken
 
 
 

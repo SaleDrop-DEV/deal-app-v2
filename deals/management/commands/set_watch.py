ae66b54f4f94d323819dc4f2bc3d350f05426717
SaleDrop-DEV <u4987625257@gmail.com>
2025-08-14 17:16:28 +0200

    Update set_watch.py

--- deals/management/commands/set_watch.py
+++ deals/management/commands/set_watch.py
@@ -1,5 +1,6 @@
-from .fetch_emails import activate_gmail_watch as activate_gmail_watch_general
-from .fetch_emails_F import activate_gmail_watch as activate_gmail_watch_female
+# from .fetch_emails import activate_gmail_watch as activate_gmail_watch_general
+# from .fetch_emails_F import activate_gmail_watch as activate_gmail_watch_female
+from .models import GmailToken
 from django.core.management.base import BaseCommand
 
 TOPIC_NAME_general = "projects/deals-app-465712/topics/gmail-notifications"
@@ -10,8 +11,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            activate_gmail_watch_general(user_id='gijsgprojects@gmail.com', topic_name=TOPIC_NAME_general)
-            activate_gmail_watch_female(user_id='donnapatrona79@gmail.com', topic_name=TOPIC_NAME_female)
+            GmailToken.objects.get(name="gijsgprojects@gmail.com").activate_gmail_watch(user_id="gijsgprojects@gmail.com", topic_name=TOPIC_NAME_general)
+            GmailToken.objects.get(name="donnapatrona79@gmail.com").activate_gmail_watch(user_id="donnapatrona79@gmail.com", topic_name=TOPIC_NAME_female)
+            # activate_gmail_watch_general(user_id='gijsgprojects@gmail.com', topic_name=TOPIC_NAME_general)
+            # activate_gmail_watch_female(user_id='donnapatrona79@gmail.com', topic_name=TOPIC_NAME_female)
         except Exception as e:
             self.stderr.write(self.style.ERROR(f'Error activating Gmail watch: {e}'))
         else:

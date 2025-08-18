580f4196d00f186fcc075b457bf850b5082a1e4c
SaleDrop-DEV <u4987625257@gmail.com>
2025-08-18 17:49:16 +0200

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -73,10 +73,10 @@ def for_business(request):
         })
 
 def custom_404_view(request, exception):
-    return render(request, '404.html', {}, status=404)
+    return render(request, 'pages/404.html', {}, status=404)
 
 def custom_500_view(request):
-    return render(request, '500.html', status=500)
+    return render(request, 'pages/500.html', status=500)
 
 
 def theme_designer(request):

a733f63b82558294d2abcac11d4fe0138df26587
SaleDrop-DEV <u4987625257@gmail.com>
2025-09-05 11:37:17 +0200

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import CustomUser, ExtraUserInformation
+
+admin.site.register(CustomUser)
+admin.site.register(ExtraUserInformation)
+
+
 # Register your models here.

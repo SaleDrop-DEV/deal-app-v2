5221569058b567ad9340756435832cd884dcc3cc
SaleDrop-DEV <u4987625257@gmail.com>
2025-09-09 10:11:22 +0200

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import API_Errors
+from .models import API_Errors, API_Errors_Site
 
 admin.site.register(API_Errors)
+admin.site.register(API_Errors_Site)
+
+
 
 # Register your models here.

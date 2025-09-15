2f885b16763215ca81906759af092e806cfa1048
Gijs <gijsgreendyke@gmail.com>
2025-09-15 12:32:17 +0200

    include forms

--- pages/forms.py
+++ pages/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import BusinessRequest
+from .models import BusinessRequest, StaticContent
 
 class BusinessRequestForm(forms.ModelForm):
     message = forms.CharField(
@@ -13,3 +13,22 @@ class BusinessRequestForm(forms.ModelForm):
         model = BusinessRequest
         fields = ['store_name', 'store_email', 'store_phone_number', 'message']
 
+class StaticContentForm(forms.ModelForm):
+    # Override image_url to use FileField (so you can upload an image)
+    image_url = forms.ImageField(required=False)
+
+    class Meta:
+        model = StaticContent
+        fields = ['content_name', 'dimensions', 'image_url']
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        # If a file was uploaded, update the image_url with the media path
+        uploaded_file = self.cleaned_data.get('image_url')
+        if uploaded_file:
+            instance.image_url = f"media/{uploaded_file.name}"
+
+        if commit:
+            instance.save()
+        return instance

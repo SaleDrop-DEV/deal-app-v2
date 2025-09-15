21c0b4a5b2d206a43e0c4706d3069665dbab0cbc
Gijs <gijsgreendyke@gmail.com>
2025-09-15 12:56:35 +0200

    fix content manager

--- pages/forms.py
+++ pages/forms.py
@@ -21,14 +21,14 @@ class StaticContentForm(forms.ModelForm):
         model = StaticContent
         fields = ['content_name', 'dimensions', 'image_url']
 
-    def save(self, commit=True):
-        instance = super().save(commit=False)
+    # def save(self, commit=True):
+    #     instance = super().save(commit=False)
 
-        # If a file was uploaded, update the image_url with the media path
-        uploaded_file = self.cleaned_data.get('image_url')
-        if uploaded_file:
-            instance.image_url = f"media/{uploaded_file.name}"
+    #     # If a file was uploaded, update the image_url with the media path
+    #     uploaded_file = self.cleaned_data.get('image_url')
+    #     if uploaded_file:
+    #         instance.image_url = f"media/{uploaded_file.name}"
 
-        if commit:
-            instance.save()
-        return instance
+    #     if commit:
+    #         instance.save()
+    #     return instance

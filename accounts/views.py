2118e772fd4d69604af126795b62c6fe228781ad
Gijs <gijsgreendyke@gmail.com>
2025-09-26 13:52:44 +0200

    After finishing registration ask user for going to the app

--- accounts/views.py
+++ accounts/views.py
@@ -195,7 +195,7 @@ def complete_profile(request):
             extra_info.save()
             
             # Redirect to the main page after successful submission
-            return redirect(f"{reverse('stores')}?succesfuly_activated=1")
+            return redirect(f"{reverse('stores')}?succesfuly_activated=1&go_to_app=1")
         else:
             # If the submission is invalid, show an error.
             error = "Selecteer een geldige optie."

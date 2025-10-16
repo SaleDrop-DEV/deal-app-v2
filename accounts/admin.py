1cdf56de1cb67e069ef917834824a700cc1e835e
Gijs <gijsgreendyke@gmail.com>
2025-10-16 16:40:33 +0200

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,8 @@ from .models import CustomUser, ExtraUserInformation, Device
 
 admin.site.register(CustomUser)
 admin.site.register(ExtraUserInformation)
+admin.site.register(Device)
+
 
 
 # Register your models here.
